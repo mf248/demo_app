0e67a337e6d4c2903f98236a8167ef5b9d41c1c7
Mike Fergus <mfergus48@gmail.com>
2012-05-31 23:47:07 -0500

    added tildas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0de2d59e3ae5c6709a04d47bdc7533af3e133acd567c99bed254c77b5d0ec1e4fc4dc022993832d737b6677110e3a0de202665a55c8a68d8f81f264366abeffd'
